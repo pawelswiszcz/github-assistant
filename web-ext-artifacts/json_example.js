71bdc9de7522eb23f20a623bc5e90f549c45142b
Paweł Świszcz <pawel.swiszcz@gmail.com>
2023-09-08 14:33:00 +0200

    Create json_example.js

---
+++ web-ext-artifacts/json_example.js
@@ -0,0 +1,61 @@
+{
+  "actions": [
+    {
+      "row": [
+        {
+          "title": "<i class='bi bi-file-earmark-text'></i> PR description template",
+          "target": "#pr-description",
+          "action": "## Links\r\n - \r\n\r\n## Description\r\n\r\n**Why**: [describe why this PR exists]\r\n\r\n**How**: [describe how this PR works]\r\n\r\n<details><summary>screenshot</summary><p>\r\n[add image]\r\n</p></details>\r\n\r\n## Who might be interested\r\n\r\nUXFCP\r\n\r\n## Testing Recommendations\r\n\r\n- [ ] test case I\r\n- [ ] taste case II\r\n"
+        }
+      ],
+      "row2": [
+        {
+          "title": "<i class='bi bi-chat-right-dots'></i> suggestion (non-blocking)",
+          "action": "**suggestion (non-blocking):**",
+          "target": "#pr-comment"
+        },
+        {
+          "title": "<i class='bi bi-screwdriver'></i> nitpick",
+          "action": "**nitpick:**",
+          "target": "#pr-comment"
+        },
+        {
+          "title": "<i class='bi bi-file-earmark'></i> note",
+          "action": "**note:**",
+          "target": "#pr-comment"
+        },
+        {
+          "title": "<i class='bi bi-patch-question-fill'></i> question",
+          "action": "**question:**",
+          "target": "#pr-comment"
+        }
+      ],
+      "row3": [
+        {
+          "title": "<i class='bi bi-star-fill'></i> praise",
+          "action": "**praise:**",
+          "target": "#pr-comment"
+        }
+      ],
+      "row4": [
+        {
+          "title": "<i class='bi bi-shield-fill-exclamation'></i> issue",
+          "action": "**issue:**",
+          "target": "#pr-comment"
+        }
+      ],
+      "row5": [
+        {
+          "title": "<i class='bi bi-person-bounding-box'></i> Author",
+          "target": "#pr-comment",
+          "action": "@#mrAuthor#"
+        },
+        {
+          "title": "<i class='bi bi-person-square'></i> Logged user",
+          "target": "#pr-comment",
+          "action": "@#loggedUser#"
+        }
+      ]
+    }
+  ]
+}
